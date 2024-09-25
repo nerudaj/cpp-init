8f963758a432b41b23ab5a32538efdd5f1dc22b3
Jakub Neruda <nerudaj@email.cz>
2024-09-25 21:15:55 +0200

    Add version to get_cpp_init.cmake

--- cmake/get_cpp_init.cmake
+++ cmake/get_cpp_init.cmake
@@ -4,7 +4,7 @@ function ( fetch_cpp_init )
     FetchContent_Declare (
         cpp-init
         GIT_REPOSITORY https://github.com/nerudaj/cpp-init
-        GIT_TAG "main"
+        GIT_TAG "v0.1.1"
     )
 
     FetchContent_MakeAvailable ( cpp-init )
