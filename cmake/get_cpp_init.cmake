b71de373d72db9ab154d5063d9080264847c737b
Jakub Neruda <nerudaj@email.cz>
2024-10-14 09:45:01 +0200

    Update get_cpp_init.cmake

--- cmake/get_cpp_init.cmake
+++ cmake/get_cpp_init.cmake
@@ -5,32 +5,39 @@
 # Example:
 #   fetch_cpp_init()
 #   fetch_cpp_init(SCRIPT_DIR "/path/to/scripts")
-function(fetch_cpp_init)
-    set(oneValueArgs SCRIPT_DIR)
-    cmake_parse_arguments(FETCH_CPP_INIT "" "${oneValueArgs}" "" ${ARGN})
+function ( fetch_cpp_init )
+    set ( oneValueArgs SCRIPT_DIR )
+    cmake_parse_arguments ( FETCH_CPP_INIT "" "${oneValueArgs}" "" ${ARGN} )
 
-    include(FetchContent)
+    include ( FetchContent )
     FetchContent_Declare(
         cpp-init
         GIT_REPOSITORY https://github.com/nerudaj/cpp-init
-        GIT_TAG "v0.2.0"
+        GIT_TAG "main"
     )
-    FetchContent_MakeAvailable(cpp-init)
+    FetchContent_MakeAvailable ( cpp-init )
     
-    if(FETCH_CPP_INIT_SCRIPT_DIR)
-        set(CPPINIT_FOLDER "${FETCH_CPP_INIT_SCRIPT_DIR}" PARENT_SCOPE)
-        file(COPY "${cpp-init_SOURCE_DIR}/cmake/cpp.cmake" DESTINATION "${CPPINIT_FOLDER}")
-        file(COPY "${cpp-init_SOURCE_DIR}/cmake/bootstrap.cmake" DESTINATION "${CPPINIT_FOLDER}")
-    else()
-        set(CPPINIT_FOLDER "${cpp-init_SOURCE_DIR}/cmake" PARENT_SCOPE)
-    endif()
+    if ( FETCH_CPP_INIT_SCRIPT_DIR )
+        set ( CPPINIT_FOLDER "${FETCH_CPP_INIT_SCRIPT_DIR}" )
+        file ( COPY_FILE
+            "${cpp-init_SOURCE_DIR}/cmake/cpp.cmake"
+            "${CPPINIT_FOLDER}/cpp.cmake"
+        )
+        file ( COPY_FILE
+            "${cpp-init_SOURCE_DIR}/cmake/bootstrap.cmake"
+            "${CPPINIT_FOLDER}/bootstrap.cmake"
+        )
+    else ()
+        set( CPPINIT_FOLDER "${cpp-init_SOURCE_DIR}/cmake" )
+    endif ()
     
-    set ( CLANG_FORMAT_PATH "${cpp-init_SOURCE_DIR}/.clang-format PARENT_SCOPE )
-    set ( CLANG_TIDY_PATH "${cpp-init_SOURCE_DIR}/.clang-tidy PARENT_SCOPE )
+    set ( CLANG_FORMAT_PATH "${cpp-init_SOURCE_DIR}/.clang-format" PARENT_SCOPE )
+    set ( CLANG_TIDY_PATH "${cpp-init_SOURCE_DIR}/.clang-tidy" PARENT_SCOPE )
+
+    return( PROPAGATE CPPINIT_FOLDER )
 endfunction()
 
+fetch_cpp_init ( SCRIPT_DIR "${CMAKE_CURRENT_SOURCE_DIR}/cmake" )
 
-fetch_cpp_init ()
-
-include ( "${CPPINIT_FOLDER}/bootstrap.cmake" )
 include ( "${CPPINIT_FOLDER}/cpp.cmake" )
+include ( "${CPPINIT_FOLDER}/bootstrap.cmake" )
